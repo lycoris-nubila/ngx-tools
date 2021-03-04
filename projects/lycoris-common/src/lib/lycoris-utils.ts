56998d7af18d9f4833841e398c0ebd7f65214f62
Sébastien Doubey <sebastien.doubey@gmail.com>
2021-03-04 20:54:32 +0100

    Add isEquals

--- projects/lycoris-common/src/lib/lycoris-utils.ts
+++ projects/lycoris-common/src/lib/lycoris-utils.ts
@@ -7,21 +7,26 @@ import {IEntity} from './models/entity';
 import {RemoteData} from './models/remote-data';
 import {RemoteDataPage} from './models/remote-data-page';
 import isDate from 'lodash-es/isDate';
+import isEqual from 'lodash-es/isEqual';
 import {DateTime} from 'luxon';
 
+export function isEquals(a: any, b: any) {
+  return isEqual((a ?? null), (b ?? null));
+}
+
 export function localeDateToDateTime(
-    date: Date, timeZone: string): DateTime {
+  date: Date, timeZone: string): DateTime {
   return isDate(date) ?
-      DateTime.fromISO(DateTime.fromJSDate(date).toISO({includeOffset: false}),
-          {zone: timeZone}) :
-      DateTime.local().setZone(timeZone);
+    DateTime.fromISO(DateTime.fromJSDate(date).toISO({includeOffset: false}),
+      {zone: timeZone}) :
+    DateTime.local().setZone(timeZone);
 }
 
 export function timestampToLocaleDate(
-    timestamp: number, timeZone: string): Date {
+  timestamp: number, timeZone: string): Date {
   return !!timestamp ?
-      dateTimeToLocaleDate(DateTime.fromMillis(timestamp, {zone: timeZone})) :
-      new Date();
+    dateTimeToLocaleDate(DateTime.fromMillis(timestamp, {zone: timeZone})) :
+    new Date();
 }
 
 export function dateToLocaleDate(date: Date, timeZone: string): Date {
@@ -30,68 +35,68 @@ export function dateToLocaleDate(date: Date, timeZone: string): Date {
 
 export function dateTimeToLocaleDate(dateTime: DateTime): Date {
   return dateTime.isValid ?
-      new Date(dateTime.toISO({includeOffset: false})) :
-      new Date();
+    new Date(dateTime.toISO({includeOffset: false})) :
+    new Date();
 }
 
 export function compareEntity<T extends IEntity>(document1: T, document2: T) {
   if (!Array.isArray(document1) && !Array.isArray(document2)) {
     return JSON.stringify(omitBy(document1, (v, k) => k.endsWith('$'))) ===
-        JSON.stringify(omitBy(document2, (v, k) => k.endsWith('$')));
+      JSON.stringify(omitBy(document2, (v, k) => k.endsWith('$')));
   }
 }
 
 export function compareEntities<T extends IEntity>(
-    array1: T[], array2: T[]): boolean {
+  array1: T[], array2: T[]): boolean {
   return isEqualWith(array1, array2, (d1, d2) => compareEntity(d1, d2));
 }
 
 export function appData<T1, T2>(
-    a: Data$<T1>, b: Observable<T2>, throttle = 250): Data$<T2> {
+  a: Data$<T1>, b: Observable<T2>, throttle = 250): Data$<T2> {
   return combineLatest([a, b]).pipe(observeOn(asyncScheduler),
-      map(([api, entity]) => ({
-        fresh: api.fresh,
-        data: entity,
-      } as RemoteData<T2>)),
-      throttleTime(throttle, asyncScheduler, {leading: true, trailing: true}),
-      defaultIfEmpty({data: null as T2, fresh: true}));
+    map(([api, entity]) => ({
+      fresh: api.fresh,
+      data: entity,
+    } as RemoteData<T2>)),
+    throttleTime(throttle, asyncScheduler, {leading: true, trailing: true}),
+    defaultIfEmpty({data: null as T2, fresh: true}));
 }
 
 export function appEntity<T1, T2 extends IEntity>(
-    a: Data$<T1>, b: Observable<T2>, throttle = 250): Data$<T2> {
+  a: Data$<T1>, b: Observable<T2>, throttle = 250): Data$<T2> {
   return combineLatest([a, b]).pipe(observeOn(asyncScheduler),
-      map(([api, entity]) => ({
-        fresh: api.fresh,
-        data: entity,
-      } as RemoteData<T2>)),
-      throttleTime(throttle, asyncScheduler, {leading: true, trailing: true}),
-      defaultIfEmpty({data: null as T2, fresh: true}));
+    map(([api, entity]) => ({
+      fresh: api.fresh,
+      data: entity,
+    } as RemoteData<T2>)),
+    throttleTime(throttle, asyncScheduler, {leading: true, trailing: true}),
+    defaultIfEmpty({data: null as T2, fresh: true}));
 }
 
 export function appEntities<T1, T2 extends IEntity>(
-    a: Data$<T1[]>, b: Observable<T2[]>, throttle = 250): Data$<T2[]> {
+  a: Data$<T1[]>, b: Observable<T2[]>, throttle = 250): Data$<T2[]> {
   return combineLatest([a, b]).
-      pipe(observeOn(asyncScheduler), map(([api, entities]) => ({
-            totalElements: Math.max(api.data?.length ?? 0, entities.length),
-            fresh: api.fresh,
-            data: (entities || []),
-          } as RemoteData<T2[]>)), throttleTime(throttle, asyncScheduler,
-          {leading: true, trailing: true}),
-          defaultIfEmpty(
-              {data: [], fresh: true, totalElements: 0} as RemoteData<T2[]>));
+    pipe(observeOn(asyncScheduler), map(([api, entities]) => ({
+        totalElements: Math.max(api.data?.length ?? 0, entities.length),
+        fresh: api.fresh,
+        data: (entities || []),
+      } as RemoteData<T2[]>)), throttleTime(throttle, asyncScheduler,
+      {leading: true, trailing: true}),
+      defaultIfEmpty(
+        {data: [], fresh: true, totalElements: 0} as RemoteData<T2[]>));
 }
 
 export function appEntitiesPage<T1, T2 extends IEntity>(
-    a: DataPage$<T1>,
-    b: Observable<T2[]>,
-    throttle = 250): DataPage$<T2> {
+  a: DataPage$<T1>,
+  b: Observable<T2[]>,
+  throttle = 250): DataPage$<T2> {
   return combineLatest([a, b]).
-      pipe(observeOn(asyncScheduler), map(([api, entities]) => ({
-            totalElements: Math.max(api.totalElements, entities.length),
-            fresh: api.fresh,
-            data: (entities || []),
-          } as RemoteData<T2[]>)), throttleTime(throttle, asyncScheduler,
-          {leading: true, trailing: true}),
-          defaultIfEmpty(
-              {data: [], fresh: true, totalElements: 0} as RemoteDataPage<T2>));
+    pipe(observeOn(asyncScheduler), map(([api, entities]) => ({
+        totalElements: Math.max(api.totalElements, entities.length),
+        fresh: api.fresh,
+        data: (entities || []),
+      } as RemoteData<T2[]>)), throttleTime(throttle, asyncScheduler,
+      {leading: true, trailing: true}),
+      defaultIfEmpty(
+        {data: [], fresh: true, totalElements: 0} as RemoteDataPage<T2>));
 }
